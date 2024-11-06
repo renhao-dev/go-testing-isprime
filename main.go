71d57146dffa0a823caa46f28cd89c77f41fe133
unknown <sam.haff.rn@gmail.com>
2024-11-06 02:27:01 +0200

    rewrite into CLI and add corresponding tests

--- main.go
+++ main.go
@@ -1,13 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	n := 2
+	intro()
+
+	done := make(chan bool)
+
+	go checkUserInput(done)
+
+	<-done
+	close(done)
+
+	fmt.Println("Thanks!")
+}
+
+func checkUserInput(done chan bool) {
+	input := bufio.NewScanner(os.Stdin)
+
+	for {
+		msg, exit := checkNumbers(input)
+
+		if exit {
+			done <- true
+			return
+		}
+
+		fmt.Println(msg)
+
+		prompt()
+	}
+}
+
+func checkNumbers(input *bufio.Scanner) (string, bool) {
+	input.Scan()
+
+	inputText := input.Text()
+
+	if strings.EqualFold(inputText, "q") {
+		return "", true
+	}
+
+	n, err := strconv.Atoi(inputText)
+	if err != nil {
+		return "Please, enter a valid number", false
+	}
 
 	_, msg := isPrime(n)
 
-	fmt.Println(msg)
+	return msg, false
+}
+
+func intro() {
+	fmt.Println("Is number a prime?")
+	fmt.Println("------------------")
+
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("Enter your number -> ")
 }
 
 func isPrime(n int) (bool, string) {
--- main_test.go
+++ main_test.go
@@ -1,6 +1,12 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
 
 func Test_isPrime(t *testing.T) {
 	tests := []struct {
@@ -10,7 +16,10 @@ func Test_isPrime(t *testing.T) {
 		expectedMsg string
 	}{
 		{"prime", 7, true, "7 is prime"},
-		{"prime", 6, true, "6 is prime"},
+		{"not prime", 6, false, "6 is divisable by 2"},
+		{"zero", 0, false, "0 is not prime, by def"},
+		{"one", 1, false, "1 is not prime, by def"},
+		{"negative", -11, false, "Negative numbers are not prime by def"},
 	}
 
 	for _, test := range tests {
@@ -22,4 +31,73 @@ func Test_isPrime(t *testing.T) {
 			t.Errorf("%s Expected msg: %s, got: %s", test.name, test.expectedMsg, msg)
 		}
 	}
+}
+func Test_intro(t *testing.T) {
+	r, w, _ := os.Pipe()
+
+	oldOut := os.Stdout
+	os.Stdout = w
+
+	intro()
+
+	_ = w.Close()
+	text, _ := io.ReadAll(r)
+	output := string(text)
+
+	expectedOutputSubstr := "Enter your number -> "
+	if !strings.Contains(output, expectedOutputSubstr) {
+		t.Errorf("Incorrect intro text, got '%s'", output)
+	}
+
+	os.Stdout = oldOut
+}
+func Test_prompt(t *testing.T) {
+	r, w, _ := os.Pipe()
+
+	oldOut := os.Stdout
+	os.Stdout = w
+
+	prompt()
+
+	_ = w.Close()
+	text, _ := io.ReadAll(r)
+	output := string(text)
+
+	expectedOutput := "Enter your number -> "
+	if output != expectedOutput {
+		t.Errorf("Invalid output expected: '%s', got '%s'", expectedOutput, output)
+	}
+
+	os.Stdout = oldOut
+}
+
+func Test_checkNumbers(t *testing.T) {
+	tests := []struct {
+		name           string
+		input          string
+		expectedMsg    string
+		expectedIfDone bool
+	}{
+		{"prime", "7", "7 is prime", false},
+
+		{"not prime", "6", "6 is divisable by 2", false},
+		{"quit", "q", "", true},
+		{"not number", "balalalaika.7", "Please, enter a valid number", false},
+		{"prime", "7", "7 is prime", false},
+	}
+
+	for _, test := range tests {
+		input := strings.NewReader(test.input)
+		scanner := bufio.NewScanner(input)
+
+		msg, done := checkNumbers(scanner)
+
+		if msg != test.expectedMsg {
+			t.Errorf("Expected output msg: %s, got: %s", test.expectedMsg, msg)
+		}
+		if done != test.expectedIfDone {
+			t.Errorf("Expected is done: %t, got: %t", test.expectedIfDone, done)
+		}
+	}
+
 }
